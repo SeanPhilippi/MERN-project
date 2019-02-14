803e8edf3aef93c23bf4bff90093a56b121c02e9
Sean Philippi <sean.philippi@gmail.com>
2019-02-14 13:31:04 -0600

    added IssueRow and IssueTable

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,39 @@ class IssueFilter extends React.Component {
   }
 }
 
+class IssueRow extends React.Component {
+  render() {
+    const borderedStyle = {border: "1px solid silver", padding: 4};
+    return (
+      <tr>
+        <td style={borderedStyle}>this.props.issue_id</td>
+        <td style={borderedStyle}>this.props.issue_title</td>
+      </tr>
+    )
+  }
+}
+
 class IssueTable extends React.Component {
   render() {
+    const borderedStyle = {border: "1px solid silver", padding 6};
     return (
-      <div>This is a placeholder for a table of Issues.</div>
+      <table style={{borderCollapse: "collapse"}}>
+        <thread>
+          <tr>
+            <th style={borderedStyle}>Id</th>
+            <th style={borderedStyle}>Title</th>
+          </tr>
+        </thread>
+        <tbody>
+          <IssueRow issue_id={1} >
+            Error in console when clicking Add
+          </IssueRow>
+          <IssueRow issue_id={2} 
+            Missing bottom border on panel>
+          </IssueRow>
+        </tbody>
+      </table>
+      
     )
   }
 }
